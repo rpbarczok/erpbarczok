0195a0ef20315c27870908ee914be8f714020474
Ralph <ralph.barczok@gmx.de>
2025-02-11 15:18:04 +0100

    openapiSpec in client 7

--- client/utils/openapiclientaxios.ts
+++ client/utils/openapiclientaxios.ts
@@ -1,8 +1,7 @@
 import {OpenAPIClientAxios} from 'openapi-client-axios'
 import {Client} from "../types/openapi.d.js"
-import path from 'node:path'
 
-const api = new OpenAPIClientAxios({ definition: path.join(import.meta.dirname, 'client', 'public', 'openapi.json')})
+const api = new OpenAPIClientAxios({ definition: 'client/public/openapi.json'})
 
 
 export const client = await api.init<Client>()
