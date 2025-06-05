7d0cdec4fb82f25829d929a280ac0b6e058fb00d
ralph <ralph@ubuntu.fritz.box>
2025-06-05 14:17:54 +0000

    adjusted test_db build

---
+++ server/typescript/tests/addressTypes.integration.test.ts
@@ -0,0 +1,447 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import './utils/env.test.js'
+import request from 'supertest'
+import { startingApp } from '../app.js'
+import { sequelize } from '../models/index.js'
+import { expect } from 'expect'
+import { App } from 'supertest/types.js'
+import { sha256 } from './utils/hasher.js'
+import jwt from 'jsonwebtoken'
+
+const iat = Math.floor(Date.now() / 1000)
+const iatADayAgo = iat - 60 * 60 * 24
+const iatAWeekAgo = iat - 8 * 60 * 60 * 24
+
+const audience = process.env.AUDIENCE ?? 'www.example.com'
+const issuer = process.env.IDP_SERVER ?? 'www.example.com'
+const scope = process.env.SCOPE ?? 'openid email profile admin user'
+const secret = process.env.TEST_SECRET ?? 'secret'
+const algorithms = ['HS256']
+
+const validTokenAdmin: string = jwt.sign(
+    {
+        'iss': issuer,
+        'aud': audience,
+        'iat': iat,
+        'scope': scope,
+        'expiresIn': '1h',
+        'alg': algorithms[0],
+    },
+    secret
+)
+
+const validTokenGuest: string = jwt.sign(
+    {
+        'iss': issuer,
+        'aud': audience,
+        'iat': iat,
+        'scope': 'openid email',
+        'expiresIn': '1h',
+        'alg': algorithms[0],
+    },
+    secret
+)
+
+const validTokenUser: string = jwt.sign(
+    {
+        'iss': issuer,
+        'aud': audience,
+        'iat': iat,
+        'scope': 'openid email user',
+        'expiresIn': '1h',
+        'alg': algorithms[0],
+    },
+    secret
+)
+
+const expiredToken: string = jwt.sign(
+    {
+        'iss': issuer,
+        'aud': audience,
+        'iat': iatAWeekAgo,
+        'scope': scope,
+        'alg': algorithms[0],
+        'exp': iatADayAgo
+    },
+    secret
+)
+
+const addressTypeA = { 'name': 'Unternehmensaddresse' }
+const addressTypeB = { 'name': 'Lieferaddresse' }
+const addressTypeC = { 'name': 'Rechnungsaddresse' }
+const etagA = sha256(JSON.stringify(addressTypeA))
+const etagB = sha256(JSON.stringify(addressTypeB))
+const etagC = sha256(JSON.stringify(addressTypeC))
+
+describe('/address-types/ HTTP integration Tests', function () {
+    this.timeout(5000)
+    let app: App
+
+    before(async function () {
+        app = await startingApp
+        await sequelize.sync({ force: true })
+    });
+
+    describe('GET /address-types/ and POST /address-types/', function () {
+
+        it('GET /address-types/ should fail with 401 for missing AuthN', async function () {
+            const response = await request(app)
+                .get('/address-types/')
+                .set('Accept', 'application/json')
+                .expect('content-type', /json/)
+                .expect(401)
+            expect(response.headers['content-type']).toMatch(/json/)
+            expect(response.status).toEqual(401)
+            expect(response.body.status).toEqual(401)
+            expect(response.body.message).toEqual('No authorization token was found')
+        })
+
+        it('GET /address-types/ should fail with 401 for expired AuthN', async function () {
+            const response = await request(app)
+                .get('/address-types/')
+                .set('Accept', 'application/json')
+                .set('Authorization', `Bearer ${expiredToken}`)
+                .expect('content-type', /json/)
+                .expect(401)
+            expect(response.headers['content-type']).toMatch(/json/)
+            expect(response.status).toEqual(401)
+            expect(response.body.status).toEqual(401)
+            expect(response.body.message).toEqual('jwt expired')
+        })
+
+        it('GET /address-types/ should succeed with 200 and return [] for a fresh and empty DB as Guest', async function () {
+            const response = await request(app)
+                .get('/address-types/')
+                .set('Accept', 'application/json')
+                .set('Authorization', `Bearer ${validTokenGuest}`)
+                .expect('content-type', /json/)
+                .expect(200, [])
+            expect(response.headers['content-type']).toMatch(/json/)
+            expect(response.status).toEqual(200)
+            expect(response.body).toEqual([])
+        })
+
+        it('GET /address-types/ should succeed with 200 and return [] for a fresh and empty DB as User', async function () {
+            const response = await request(app)
+                .get('/address-types/')
+                .set('Accept', 'application/json')
+                .set('Authorization', `Bearer ${validTokenUser}`)
+                .expect('content-type', /json/)
+                .expect(200, [])
+            expect(response.headers['content-type']).toMatch(/json/)
+            expect(response.status).toEqual(200)
+            expect(response.body).toEqual([])
+        })
+
+        it('GET /address-types/ should succeed with 200 and return [] for a fresh and empty DB as Admin', async function () {
+            const response = await request(app)
+                .get('/address-types/')
+                .set('Accept', 'application/json')
+                .set('Authorization', `Bearer ${validTokenAdmin}`)
+                .expect('content-type', /json/)
+                .expect(200, [])
+            expect(response.headers['content-type']).toMatch(/json/)
+            expect(response.status).toEqual(200)
+            expect(response.body).toEqual([])
+        })
+
+        it('post /address-types with name should fail with 401 because of missing authZ ', async function () {
+            const response = await request(app)
+                .post('/address-types/')
+                .send(addressTypeA)
+                .set('Accept', 'application/json')
+                .set('Authorization', `Bearer ${validTokenUser}`)
+                .expect(401)
+            expect(response.headers['content-type']).toMatch(/json/)
+            expect(response.status).toEqual(401)
+            expect(response.body.status).toEqual(401)
+            expect(response.body.message).toEqual('unauthorized')
+        })
+
+        it('post /address-types with name should succeed for Admin', async function () {
+            const response = await request(app)
+                .post('/address-types/')
+                .send(addressTypeA)
+                .set('Accept', 'application/json')
+                .set('Authorization', `Bearer ${validTokenAdmin}`)
+                .expect(201, '')
+            expect(response.headers.location).toEqual('/address-types/1')
+            expect(response.headers.etag).toEqual(sha256(JSON.stringify(addressTypeA)))
+        })
+
+        it('Post /address-types with name', async function () {
+            const response = await request(app)
+                .post('/address-types/')
+                .send(addressTypeB)
+                .set('Accept', 'application/json')
+                .set('Authorization', `Bearer ${validTokenAdmin}`)
+                .expect(201, '')
+                .expect('location', '/address-types/2')
+            expect(response.headers.location).toEqual('/address-types/2')
+            expect(response.headers.etag).toEqual(sha256(JSON.stringify(addressTypeB)))
+        })
+
+        it('Post /address-types without name fails', async function () {
+            const response = await request(app)
+                .post('/address-types/')
+                .send({})
+                .set('Accept', 'application/json')
+                .set('Authorization', `Bearer ${validTokenAdmin}`)
+                .expect('Content-Type', /json/)
+                .expect(400)
+            expect(response.body.status).toBe(400)
+            expect(response.headers['content-type']).toMatch(/json/)
+            expect(response.body.message).toMatch(/request\/body must have required property 'name'/)
+            expect(response.body.errors).toBeInstanceOf(Array)
+        })
+
+        it('Post /address-types with extra attributes fails', async function () {
+            const response = await request(app)
+                .post('/address-types/')
+                .send({ 'name': 'Sonstiges', 'extra': 'bla' })
+                .set('Authorization', `Bearer ${validTokenAdmin}`)
+                .set('Accept', 'application/json')
+                .expect('Content-Type', /json/)
+                .expect(400)
+            expect(response.body.status).toBe(400)
+            expect(response.body.message).toMatch(/must NOT have additional properties/)
+            expect(response.body.errors).toBeInstanceOf(Array)
+        })
+
+        it('Post /address-types with invalid name type fails (array)', async function () {
+            const response = await request(app)
+                .post('/address-types/')
+                .send({ 'name': {} })
+                .set('Accept', 'application/json')
+                .set('Authorization', `Bearer ${validTokenAdmin}`)
+                .expect('Content-Type', /json/)
+                .expect(400)
+            expect(response.body.status).toBe(400)
+            expect(response.body.message).toMatch(/must be string/)
+            expect(response.body.errors).toBeInstanceOf(Array)
+        })
+
+        it('GET /address-types/ works with content', async function () {
+            const response = await request(app)
+                .get('/address-types/')
+                .set('Accept', 'application/json')
+                .set('Authorization', `Bearer ${validTokenGuest}`)
+                .expect('Content-Type', /json/)
+                .expect(200)
+            expect(response.status).toBe(200)
+            expect(response.body).toEqual(
+                [
+                    { 'meta': { 'location': '/address-types/2', 'etag': etagB }, 'data': addressTypeB },
+                    { 'meta': { 'location': '/address-types/1', 'etag': etagA }, 'data': addressTypeA }
+                ])
+        })
+    })
+
+    describe('GET /address-types/{id}', function () {
+
+        it('Get /address-type/{id} succeeds when existing', async function () {
+            const response = await request(app)
+                .get('/address-types/2')
+                .set('Accept', 'application/json')
+                .set('Authorization', `Bearer ${validTokenGuest}`)
+                .expect('Content-Type', /json/)
+                .expect(200, addressTypeB)
+            expect(response.headers.location).toEqual('/address-types/2')
+            expect(response.headers.etag).toEqual(etagB)
+        })
+
+        it('Get /address-type/{id} fails with 404 when not existing', async function () {
+            const response = await request(app)
+                .get('/address-types/17')
+                .set('Accept', 'application/json')
+                .set('Authorization', `Bearer ${validTokenGuest}`)
+                .expect('Content-Type', /json/)
+                .expect(404)
+            expect(response.body.status).toBe(404)
+            expect(response.body.message).toBe('Not found: Address type with id 17.')
+        })
+
+        it('Get /address-type/{id} fails with 400 with negative id fails', async function () {
+            const response = await request(app)
+                .get('/address-types/-1')
+                .set('Accept', 'application/json')
+                .set('Authorization', `Bearer ${validTokenGuest}`)
+                .expect('Content-Type', /json/)
+                .expect(400)
+            expect(response.body.status).toBe(400)
+            expect(response.body.message).toMatch('must be >= 1')
+        })
+
+        it('Get /address-type/{id} fails with 400 with strange id', async function () {
+            const response = await request(app)
+                .get('/address-types/foo')
+                .set('Accept', 'application/json')
+                .set('Authorization', `Bearer ${validTokenGuest}`)
+                .expect('Content-Type', /json/)
+                .expect(400)
+            expect(response.body.status).toBe(400)
+            expect(response.body.message).toMatch('must be integer')
+            expect(response.body.errors).toBeInstanceOf(Array)
+        })
+    })
+
+    describe('PUT /address-types/{id}', function () {
+
+        it('Get /address-types/{id} existing address-type succeeds', async function () {
+            const response = await request(app)
+                .get('/address-types/2')
+                .set('Accept', 'application/json')
+                .set('Authorization', `Bearer ${validTokenGuest}`)
+                .expect('Content-Type', /json/)
+                .expect(200, addressTypeB)
+            expect(response.headers.location).toEqual('/address-types/2')
+            expect(response.headers.etag).toEqual(etagB)
+        })
+
+        it('Put /address-types/{id} of existing address-type fails with 401 with Guest ', async function () {
+            const response = await request(app)
+                .put('/address-types/2')
+                .set('Accept', 'application/json')
+                .set('location', '/address-types/2')
+                .set('Authorization', `Bearer ${validTokenGuest}`)
+                .set('if-match', etagB)
+                .send(addressTypeC)
+                .expect(401)
+            expect(response.body.status).toBe(401)
+            expect(response.body.message).toMatch('unauthorized')
+            expect(response.body.errors).toBeInstanceOf(Array)
+        })
+
+        it('Put /address-types/{id} of existing address-type fails with 401 with User ', async function () {
+            const response = await request(app)
+                .put('/address-types/2')
+                .set('Accept', 'application/json')
+                .set('location', '/address-types/2')
+                .set('Authorization', `Bearer ${validTokenUser}`)
+                .set('if-match', etagB)
+                .send(addressTypeC)
+                .expect(401)
+            expect(response.body.status).toBe(401)
+            expect(response.body.message).toMatch('unauthorized')
+            expect(response.body.errors).toBeInstanceOf(Array)
+        })
+
+
+        it('Put /address-types/{id} of existing address-type succeeds with correct name change with Admin', async function () {
+            const response = await request(app)
+                .put('/address-types/2')
+                .set('Accept', 'application/json')
+                .set('Authorization', `Bearer ${validTokenAdmin}`)
+                .set('location', '/address-types/2')
+                .set('if-match', etagB)
+                .send(addressTypeC)
+                .expect(204, '')
+            expect(response.headers.location).toEqual('/address-types/2')
+            expect(response.headers.etag).toEqual(etagC)
+        })
+
+        it('Get /address-types/{id} succeeds after Name Change', async function () {
+            const response = await request(app)
+                .get('/address-types/2')
+                .set('Accept', 'application/json')
+                .set('Authorization', `Bearer ${validTokenGuest}`)
+                .expect('Content-Type', /json/)
+                .expect(200, addressTypeC)
+            expect(response.headers.location).toBe('/address-types/2')
+            expect(response.headers.etag).toBe(etagC)
+        })
+
+        it('Put /address-types/{id} on changed dataset fails with error 412', async function () {
+            const response = await request(app)
+                .put('/address-types/2')
+                .set('Accept', 'application/json')
+                .set('location', '/address-types/2')
+                .set('Authorization', `Bearer ${validTokenAdmin}`)
+                .set('if-match', etagA)
+                .send(addressTypeC)
+                .expect(412)
+            expect(response.body.status).toBe(412)
+            expect(response.body.message).toMatch('Precondition failed.')
+        })
+    })
+
+    describe('DELETE /address-types/{id}', function () {
+
+        it('DELETE /address-types/{id} existing address-types fails with 401 as Guest', async function () {
+            const response = await request(app)
+                .delete('/address-types/1')
+                .set('Accept', 'application/json')
+                .set('Authorization', `Bearer ${validTokenGuest}`)
+                .expect('Content-Type', /json/)
+                .expect(401)
+            expect(response.body.status).toBe(401)
+            expect(response.body.message).toBe('unauthorized')
+        })
+        
+        it('DELETE /address-types/{id} existing address-types fails with 401 as User', async function () {
+            const response = await request(app)
+                .delete('/address-types/1')
+                .set('Accept', 'application/json')
+                .set('Authorization', `Bearer ${validTokenUser}`)
+                .expect('Content-Type', /json/)
+                .expect(401)
+            expect(response.body.status).toBe(401)
+            expect(response.body.message).toBe('unauthorized')
+        })
+
+        it('DELETE /address-types/{id} existing address-types succeeds as Admin', async function () {
+            await request(app)
+                .delete('/address-types/1')
+                .set('Accept', 'application/json')
+                .set('Authorization', `Bearer ${validTokenAdmin}`)
+                .expect(204, '')
+        })
+
+        it('DELETE /address-types/{id} fails with 404 with nonexisting address-type', async function () {
+            const response = await request(app)
+                .delete('/address-types/1')
+                .set('Accept', 'application/json')
+                .set('Authorization', `Bearer ${validTokenAdmin}`)
+                .expect('Content-Type', /json/)
+                .expect(404)
+            expect(response.body.status).toBe(404)
+            expect(response.body.message).toBe('Not found: Address type with id 1.')
+        })
+
+        it('DELETE /address-types/{id} fails with 400 with negative id', async function () {
+            const response = await request(app)
+                .delete('/address-types/-1')
+                .set('Accept', 'application/json')
+                .set('Authorization', `Bearer ${validTokenAdmin}`)
+                .expect('Content-Type', /json/)
+                .expect(400)
+            expect(response.body.status).toBe(400)
+            expect(response.body.message).toMatch('must be >= 1')
+            expect(response.body.errors).toBeInstanceOf(Array)
+        })
+
+        it('DELETE /address-types/{id} fails with 400 with id 0', async function () {
+            const response = await request(app)
+                .delete('/address-types/0')
+                .set('Accept', 'application/json')
+                .set('Authorization', `Bearer ${validTokenAdmin}`)
+                .expect('Content-Type', /json/)
+                .expect(400)
+            expect(response.body.status).toBe(400)
+            expect(response.body.message).toMatch('must be >= 1')
+            expect(response.body.errors).toBeInstanceOf(Array)
+        })
+
+        it('DELETE /address-types/{id} fails with 400 with strange id', async function () {
+            const response = await request(app)
+                .delete('/address-types/foo')
+                .set('Accept', 'application/json')
+                .set('Authorization', `Bearer ${validTokenAdmin}`)
+                .expect('Content-Type', /json/)
+                .expect(400)
+            expect(response.body.status).toBe(400)
+            expect(response.body.message).toMatch('must be integer')
+            expect(response.body.errors).toBeInstanceOf(Array)
+        })
+    })
+})
